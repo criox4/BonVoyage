c0e96cf6f08f521378a3adae1d8e4e7ade101de4
Ashutosh Pradhan <pradhan.ashutosh111@gmail.com>
2024-06-13 10:21:49 +0530

    Homepage Done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  username: string = '';
 
+  ngOnInit(): void {
+    // Simulating user login status and username
+    this.isLoggedIn = this.checkLoginStatus();
+    this.username = this.getUsername();
+  }
+
+  checkLoginStatus(): boolean {
+    // Implement your login status check logic here
+    return true; // Change this to true to simulate a logged-in user
+  }
+
+  getUsername(): string {
+    // Implement your logic to get the username here
+    return 'Ashu'; // Simulated username
+  }
+
+  logout(): void {
+    // Implement your logout logic here
+    this.isLoggedIn = false;
+    this.username = '';
+  }
 }
